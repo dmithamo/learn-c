2b3cc2e472cd8536543c1317c31c39a786445351
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-08-11 09:59:26 +0300

    [knr] ex 1-3 and 1-4 into to control structures -  for instead of while

--- k-and-r-c/playground/tempconvert.c
+++ k-and-r-c/playground/tempconvert.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
 int main() {
-  int celc, farh;
-  int low, high, step;
+  // int celc, farh;
+  // int low, high, step;
+  //
+  // low = 5;
+  // high = 100;
+  // step = 5;
+  //
+  // celc = low;
+  // high = 100;
+  // printf("%12s %12s\n", "Celcius", "Farenheit");
+  // while (celc <= high) {
+  //   printf("%12d %12.2f\n", celc, (celc * 9.0 / 5.0) + 32);
+  //   celc += step;
+  // }
 
-  low = 5;
-  high = 100;
-  step = 5;
+  int celc;
 
-  celc = low;
-  high = 100;
   printf("%12s %12s\n", "Celcius", "Farenheit");
-  while (celc <= high) {
-    printf("%12d %12.2f\n", celc, (celc * 9.0 / 5.0) + 32);
-    celc += step;
+  for (celc = 2; celc <= 100; celc += 5) {
+    printf("%12d %12.2f\n", celc, celc * 9.0 / 5.0 + 32);
   }
   return 0;
 }
