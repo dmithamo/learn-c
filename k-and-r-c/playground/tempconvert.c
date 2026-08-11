9dda05e2c8e51d5e4ad013b2ec0385fc96786f7a
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-08-11 09:54:03 +0300

    [knr] ex 1-3 and 1-4 into to control structures

---
+++ k-and-r-c/playground/tempconvert.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int celc, farh;
+  int low, high, step;
+
+  low = 5;
+  high = 100;
+  step = 5;
+
+  celc = low;
+  high = 100;
+  printf("%12s %12s\n", "Celcius", "Farenheit");
+  while (celc <= high) {
+    printf("%12d %12.2f\n", celc, (celc * 9.0 / 5.0) + 32);
+    celc += step;
+  }
+  return 0;
+}
