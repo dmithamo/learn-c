a69a036d963ccb4ec25cd14598cda90436a8b962
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-09-02 09:18:11 +0300

    [jens] contrast width and max/min of size_t vs int

--- jens-g-modern-c/getting-started.c
+++ jens-g-modern-c/getting-started.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,8 +11,16 @@ int main(int argc, [[maybe_unused]] char *argv[argc + 1]) {
       [3] = .00007,
   };
 
-  for (size_t i = 4, iters = 0; iters < 7; --i, ++iters) {
+  for (size_t i = 4; i < 5; --i) {
     printf("element at %zu is %g, \tits square is %g\n", i, A[i], A[i] * A[i]);
   }
+
+  printf("\n");
+  printf("%-20s %20s %20s %20s\n", "TYPE", "MAX", "MIN", "WIDTH");
+  printf("%-20s %20zu %20zu %20zu\n", "size_t", SIZE_MAX, 0UL,
+         sizeof(SIZE_MAX));
+
+  printf("%-20s %20d %20d %20zu\n", "int", INT_MAX, INT_MIN, sizeof(INT_MAX));
+
   return EXIT_SUCCESS;
 }
